6aee2237696598c831e7837ba7c84ad78b306bb2
Luke Shiner <luke@lukeshiner.com>
2017-07-25 10:56:42 +0100

    Fix returned value.

--- list_input/listinput.py
+++ list_input/listinput.py
@@ -28,17 +28,21 @@ class ListInput(forms.CharField):
             return json.dumps(value)
         return value
 
-    def validate(self, value):
-        super().validate(value)
+    def to_python(self, value):
+        value = super().to_python(value)
         try:
-            json_value = json.loads(value)
+            python_value = json.loads(value)
         except ValueError:
             raise ValidationError('Not valid JSON.')
-        if not isinstance(json_value, list):
+        return python_value
+
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, list):
             raise ValidationError('Not valid JSON list.')
-        if self.minimum > 0 and len(json_value) < self.minimum:
+        if self.minimum > 0 and len(value) < self.minimum:
             raise ValidationError(
                 'At least {} value(s) required'.format(self.minimum))
-        if self.maximum > 0 and len(json_value) > self.maximum:
+        if self.maximum > 0 and len(value) > self.maximum:
             raise ValidationError(
                 'No more than {} values can be supplied'.format(self.maximum))
